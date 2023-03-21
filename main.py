2d40b409f01f13fd2402f00d6a7aa6ecf679e73f
Simon <simonrod2404@gmail.com>
2023-03-21 16:59:22 -0500

    add some env variables

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import flask
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
@@ -16,7 +18,7 @@ from flask_gravatar import Gravatar
 from flask_wtf import FlaskForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -25,7 +27,7 @@ gravatar = Gravatar(app)
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
